9216952be190466a3866671d9d7bd3956e7af414
Rakyesh24 <50745370+Rakyesh24@users.noreply.github.com>
2021-11-20 12:22:11 +0530

    Mail page and test case commit

--- DemoRediffmail/src/main/java/pages/Mail.java
+++ DemoRediffmail/src/main/java/pages/Mail.java
@@ -1,5 +1,56 @@
 package pages;
 
-public class Mail {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Mail {
+	WebDriver driver;
+	private By writemail = By.xpath("//b[contains(text(),'Write mail')]");
+	private By to = By.xpath("//input[@id='TO_IDcmp2']");
+	private By subj = By.xpath("//body/div[4]/div[2]/div[2]/div[2]/div[2]/ul[2]/li[2]/div[1]/div[1]/ul[1]/li[4]/input[1]");
+	private By frame= By.xpath("//body/div[4]/div[2]/div[2]/div[2]/div[2]/ul[2]/li[2]/div[1]/div[1]/ul[1]/li[6]/div[1]/div[1]/div[1]/iframe[1]");
+	private By body= By.xpath("//body");
+	private By send=By.xpath("//body/div[4]/div[2]/div[2]/div[2]/div[2]/ul[2]/li[2]/div[1]/div[1]/div[1]/a[1]");
+
+	public Mail(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	public void doComposeAs(String mailto, String subject,String message) {
+		try {
+			isElement(writemail).click();
+			isElement(to).sendKeys(mailto);
+			isElement(subj).sendKeys(subject);
+			driver.switchTo().frame(driver.findElement(frame));
+			Thread.sleep(10000);
+			isElement(body).sendKeys(message);
+			driver.switchTo().defaultContent();
+			isElement(send).click();
+			
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public  WebElement isElement(By by) {
+		WebDriverWait wt=new WebDriverWait(driver,30);
+		WebElement ele=null;
+		boolean flag=wt.until(ExpectedConditions.visibilityOfElementLocated(by)).isDisplayed();
+		for(int i=0;i<10;i++) {
+			if(flag) {
+				ele=driver.findElement(by);
+				break;
+			}
+			else {
+				driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			}
+		}
+		return ele;
+		
+	}
 }
--- DemoRediffmail/src/test/java/redifftest/TC_1.java
+++ DemoRediffmail/src/test/java/redifftest/TC_1.java
@@ -1,9 +1,33 @@
 package redifftest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.Login;
+import pages.Mail;
+
 public class TC_1 {
+	WebDriver driver;
+	Login lp;
+	Mail mp;
   @Test
   public void f() {
+	  String mailto="ash06borde@gmail.com";
+	  String subject="hi";
+	  String message="Sending mail from selenium webdriver";
+	  mp.doComposeAs(mailto, subject, message);
+  }
+  
+  @BeforeTest
+  public void setup() {
+	  WebDriverManager.chromedriver().setup();
+	  driver=new ChromeDriver();
+	  lp=new Login(driver);
+	  lp.openUrl();
+	  lp.doLoginAs("your username", "your password");
+	  mp=new Mail(driver);
   }
 }
